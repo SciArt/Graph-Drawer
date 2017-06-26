23dee0fcbd5a06a7383ad1b7df9b8e9ce54edd47
Gerard Wypych <science.artificer@gmail.com>
2017-06-26 02:42:45 +0200

    Visual Studio project is created.

---
+++ VS/Graph-Drawer/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Graph-Drawer | version 0.0" << endl;
+	cout << "Hello, World!" << endl;
+	cout << "Type a char and press Enter: ";
+	cin.get();
+
+	return 0;
+}
